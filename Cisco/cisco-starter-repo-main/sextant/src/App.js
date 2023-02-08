c5a6dbbb8824a18b1e4b06525f9611a20a6b9894
YTC <60242731+ytc8717@users.noreply.github.com>
2023-02-08 00:05:51 -0800

    Task3 Update

--- Cisco/cisco-starter-repo-main/sextant/src/App.js
+++ Cisco/cisco-starter-repo-main/sextant/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
+import FetchIP from './FetchIP';
 
 function App() {
   return (
     <div className="App">
       <Banner bannerText="Sextant"/>
-      <Exhibit name="ex1"/>
+      <Exhibit name="ex1" children={() => FetchIP('ipv4')}/>
       <Exhibit name="ex2"/>
       <Exhibit name="ex2"/>
     </div>
